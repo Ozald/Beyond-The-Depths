b399a0ee2264c0082b4867f228458631876faaae
Giovanni Hernandez <168580854+Bartokens@users.noreply.github.com>
2026-03-17 15:47:12 -0400

    Update PlayButton.cs

--- Assets/Data/PlayButton.cs
+++ Assets/Data/PlayButton.cs
@@ -10,16 +10,19 @@ public class ClickExample : MonoBehaviour
     // change the scene name later once we have it.
     public string sceneName = "example";
     public Button yourButton;
+    public Canvas mainMenu;
+    public Canvas settings;
 
     void Start()
     {
-        Button btn = yourButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        yourButton.onClick.AddListener(TaskOnClick);
     }
 
     void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
         SceneManager.LoadScene(sceneName);
+        mainMenu.gameObject.SetActive(false);
+        settings.gameObject.SetActive(true);
     }
 }
