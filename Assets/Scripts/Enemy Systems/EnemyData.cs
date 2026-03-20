5456776df823f7f0581231aac484b71dc3e6d73a
Osvaldo-Q <ozald8600@gmail.com>
2026-03-20 18:49:33 -0400

    Enemy Refactoring Completed

---
+++ Assets/Scripts/Enemy Systems/EnemyData.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum TransitionMode : byte
+{
+    FirstValid,         // The first valid transition found will be picked
+    Random,             // A random valid transition would be picked disregarding its probability weights
+    WeightedRandom      // A random valid transition would be picked based on its probability weights
+}
+
+[Serializable]
+public struct TransitionDestination
+{
+    public int weight;
+    public AIState toState;
+    public Condition condition;
+}
+
+[Serializable]
+public struct EnemyStateTransition
+{
+    public TransitionMode transitionMode;
+    public AIState fromState;
+    public List<TransitionDestination> destinations;
+}
+
+[CreateAssetMenu(menuName = "Enemy AI/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public AIState initalState;
+    public List<EnemyStateTransition> transitions;
+}
