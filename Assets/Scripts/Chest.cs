1809c5e0e2c7d093b4956f3c79d665f7ae1f78b8
Daniel H <danielh8533@gmail.com>
2026-03-20 15:26:54 -0400

    Chest implementation YAY

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : MonoBehaviour
+public class Chest : Interactable
 {
+    public List<Weapon> weaponList = new List<Weapon>();
+    public bool isOpen = false;
+    public override void Interact(PlayerInteraction player)
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+
+            this.gameObject.SetActive(false);
+            int randomIndex = Random.Range(0, weaponList.Count);
+            Instantiate(weaponList[randomIndex], this.gameObject.transform.position, this.gameObject.transform.rotation);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
