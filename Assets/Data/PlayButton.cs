d41ebacca0772c1428067ec2dbe2f6170db6b39b
Daniel H <danielh8533@gmail.com>
2026-04-13 16:22:53 -0400

    Scene fade transitions added

--- Assets/Data/PlayButton.cs
+++ Assets/Data/PlayButton.cs
@@ -12,15 +12,28 @@ public class PlayButton : MonoBehaviour
     public Button yourButton;
     public Canvas mainMenu;
     public Canvas settings;
+    public Animator fadeAnimator;
 
     void Start()
     {
+        fadeAnimator = Fade.instance.GetComponent<Animator>();
         yourButton.onClick.AddListener(TaskOnClick);
     }
 
     void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
+        StartCoroutine(FadeTransition());
+    }
+
+    private IEnumerator FadeTransition()
+    {
+        fadeAnimator.SetTrigger("Transition");
+
+        // Wait for animation to finish
+        yield return new WaitForSecondsRealtime(1f);
+
         SceneManager.LoadScene(sceneName);
     }
+
 }
