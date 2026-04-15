802a5b8876673e10f55eaafda4d0cd4934418a7a
Osvaldo Q. <ozald8600@gmail.com>
2026-04-15 04:02:30 -0400

    Reworked Attacks and Enemy Invincibility

--- Assets/Scripts/Enemy Systems/AttackHitboxData.cs
+++ Assets/Scripts/Enemy Systems/AttackHitboxData.cs
@@ -10,11 +10,14 @@ public class AttackHitboxData : MonoBehaviour
     public float maxLifetime;
     public int damage;
     public int knockback;
+    public bool destroyOnHit = false;
 
     public string tagToHit;
 
     private float currLifetime;
     private Rigidbody2D rb;
+
+    private HashSet<EnemyHP> enemiesHit = new HashSet<EnemyHP>();
 
     void Start()
     {
@@ -34,6 +37,32 @@ public class AttackHitboxData : MonoBehaviour
         if (transform.parent != null)
             Destroy(transform.parent.gameObject);
         else
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        EnemyHP enemy = collider.gameObject.GetComponent<EnemyHP>();
+        // StatsManager player = collider.gameObject.GetComponent<StatsManager>();
+
+        if (!collider.gameObject.CompareTag(tagToHit))
+        {
+            if (destroyOnHit)
+                Destroy(gameObject);
+
+            return;
+        }
+
+        if (enemy != null)
+        {
+            if (enemiesHit.Contains(enemy) || enemy.timeSinceLastHit < enemy.enemyData.invincibilityCooldown)
+                return;
+
+            enemy.TakeDamage(damage, knockback);
+            enemiesHit.Add(enemy);
+        }
+
+        if (destroyOnHit)
             Destroy(gameObject);
     }
 }
