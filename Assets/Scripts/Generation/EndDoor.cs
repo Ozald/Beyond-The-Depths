f21a6d0a9e7b735e42c3f087eba68c8b646358e9
Reddyboi85 <83redknights@gmail.com>
2026-04-15 23:28:06 -0400

    Bug fixes + End implementation
    Fixed idle animation bug
    Added end room and implemented end door

---
+++ Assets/Scripts/Generation/EndDoor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndDoor : MonoBehaviour
+{
+
+    public Room parentRoom;
+    public bool enabled = false;
+    public Animator fadeAnimator;
+
+    // There was a bug that cause the door transitions to loop, this fixes that
+    public bool isTransitioning;
+
+    void Start()
+    {
+        fadeAnimator = Fade.instance.GetComponent<Animator>();
+        isTransitioning = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && enabled)
+        {
+            Debug.Log("Player has entered a door");
+                StartCoroutine(FadeTransition(other));
+        }
+    }
+
+    private IEnumerator FadeTransition(Collider2D other)
+    {
+        fadeAnimator.SetTrigger("FadeOut");
+
+        // Wait for animation to finish
+        yield return new WaitForSecondsRealtime(1f);
+
+        SceneManager.LoadScene("Mainmenu");
+
+    }
+}
+
