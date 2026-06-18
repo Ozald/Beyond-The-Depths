117f622e700b2e597d8437681d87a36f2e37c039
Ill-Come-Up-With-The-Name-Later <adtmax5@gmail.com>
2026-06-18 11:57:36 -0400

    Added a breakable pot as well as a coin and heart to drop from the pot

---
+++ Assets/Scripts/Interactables/Coin.cs
@@ -0,0 +1,8 @@
+﻿public class Coin : Interactable
+{
+    public override void Interact(PlayerInteraction player)
+    {
+        player.player.GetComponent<StatsManager>().doubloons += 1;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Interactables/Pot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Pot : Interactable
+{
+    public WeightedItem[] items;
+    private ItemSelector<Interactable> LootPool;
+    public bool isOpen = false;
+    public Animator animator;
+
+    void Start()
+    {
+        LootPool = new ItemSelector<Interactable>();
+
+        foreach (WeightedItem item in items)
+            LootPool.AddItem(item.item, item.weight);
+        
+        Debug.Log("Item array size: " + items.Length);
+        Debug.Log("Loot pool items: " + LootPool.Size);
+    }
+
+    public override void Interact(PlayerInteraction player)
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+
+            this.enabled = false;
+
+            //animator.SetBool("IsOpen", true);
+
+            StartCoroutine(OpenPot());
+        }
+    }
+
+    private IEnumerator OpenPot()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        Interactable item = LootPool.Roll();
+
+        Instantiate(item, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
+        
